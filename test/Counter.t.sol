44fdb43fe56efe27a6ca512ceb14c162d8f9db1c
Sahil-Shadwal <sahilshadwal@gmail.com>
2025-08-06 12:38:00 +0530

    setting up local dev for eth development

---
+++ test/Counter.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/Counter.sol";
+
+contract TestCounter is Test {
+    Counter c;
+
+    function setUp() public {
+        c = new Counter(5);
+    }
+
+    function testIncrement() public {
+        c.increment();
+        c.increment();
+
+        assertEq(c.getNum(),7,"");
+    }
+
+    function testDecrement() public {
+        c.decrement();
+        c.decrement();
+
+        assertEq(c.getNum(),3,"");
+    }
+}
